c0738d24a1d568b82b8299c74e5c9d9187276d01
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-04-19 20:03:27 +0200

    BinaryTree finished

--- IdeaProjects/BinaryTree/src/BinaryTree/Main.java
+++ IdeaProjects/BinaryTree/src/BinaryTree/Main.java
@@ -4,5 +4,51 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Create new tree:
+        NodeClass bt = new NodeClass();
+
+        //Insert into the tree:
+        bt.insert(6);
+        bt.insert(4);
+        bt.insert(8);
+        bt.insert(3);
+        bt.insert(5);
+        bt.insert(7);
+        bt.insert(9);
+
+        //Contains the tree:
+        System.out.println("Contains 6: " + bt.containsNode(6));
+        System.out.println("Contains 4: " + bt.containsNode(4));
+        System.out.println("Contains 1: " + bt.containsNode(1));
+
+        //Remove from tree:
+        System.out.println("Contains 9 before deletion: " + bt.containsNode(9));
+        bt.delete(9);
+        System.out.println("Contains 9 after deletion: " + bt.containsNode(9));
+
+        bt.insert(9);
+
+        //In-order traversal search:
+        System.out.print("In-order:"); bt.traverseInOrder(bt.root);
+
+        System.out.println();
+
+        //Pre-order traversal search:
+        System.out.print("Pre-order:"); bt.traversePreOrder(bt.root);
+
+        System.out.println();
+
+        //Post-order traversal search:
+        System.out.print("Post-order:"); bt.traversePostOrder(bt.root);
+
+        System.out.println();
+
+        //Level-order traversal search:
+        System.out.print("Level-order:"); bt.traverseLevelOrder();
+
+        System.out.println();
+
+        //Size of the tree:
+        System.out.print("Size of the tree: " + bt.getSize());
     }
 }
--- IdeaProjects/BinaryTree/src/BinaryTree/NodeClass.java
+++ IdeaProjects/BinaryTree/src/BinaryTree/NodeClass.java
@@ -1,20 +1,23 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NodeClass {
-    private TreeNode root = null;
+
+    public TreeNode root = null;
     private TreeNode actual = null;
     private int size;
 
-
-    public TreeNode insertNode(int value, TreeNode node) {
+    private TreeNode insertNodeRecursive(int value, TreeNode node) {
         if (node == null) {
             return new TreeNode(value, null, null);
         } else {
             if (node.getValue() > value) {
-                node.setLeft(insertNode(value, node.getLeft()));
+                node.setLeft(insertNodeRecursive(value, node.getLeft()));
                 return node;
             } else if (node.getValue() < value) {
-                node.setRight(insertNode(value, node.getRight()));
+                node.setRight(insertNodeRecursive(value, node.getRight()));
                 return node;
             } else {
                 return node;
@@ -22,11 +25,127 @@ public class NodeClass {
         }
     }
 
-    public void insertNode(int value) {
+    public void insert(int value) {
         if (root == null) {
             root = new TreeNode(value, null, null);
         } else {
-            insertNode(value, root);
+            insertNodeRecursive(value, root);
         }
+    }
+
+    private boolean containsNodeRecursive(TreeNode node, int value) {
+        if (node == null) {
+            return false;
+        }
+        if (value == node.value) {
+            return true;
+        }
+        return value < node.value
+                ? containsNodeRecursive(node.left, value)
+                : containsNodeRecursive(node.right, value);
+    }
+
+    public boolean containsNode(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    private TreeNode deleteRecursive(TreeNode node, int value) {
+        if (node == null) {
+            return null;
+        }
+        if (value == node.value) {
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            if (node.left == null) {
+                return node.right;
+            }
+            findSmallestValue(root);
+            int smallestValue = findSmallestValue(node.right);
+            node.value = smallestValue;
+            node.right = deleteRecursive(node.right, smallestValue);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = deleteRecursive(node.left, value);
+            return node;
+        }
+        node.right = deleteRecursive(node.right, value);
+        return node;
+    }
+
+    private int findSmallestValue(TreeNode root) {
+        return root.left == null ? root.value : findSmallestValue(root.left);
+    }
+
+    public void delete(int value) {
+        deleteRecursive(root, value);
+    }
+
+    public void traverseInOrder(TreeNode node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder(TreeNode node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(TreeNode node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.value);
+        }
+    }
+
+    public void traverseLevelOrder() {
+        if (root == null) {
+            return;
+        }
+        Queue <TreeNode> nodes = new LinkedList <>();
+        nodes.add(root);
+
+        while (!nodes.isEmpty()) {
+            TreeNode node = nodes.remove();
+            System.out.print(" " + node.value);
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+            if (node.right != null) {
+                nodes.add(node.right);
+            }
+        }
+    }
+
+    public int getSize() {
+        if (root == null) {
+            return size;
+        }
+        Queue <TreeNode> nodes = new LinkedList <>();
+        nodes.add(root);
+        size++;
+        while (!nodes.isEmpty()) {
+            TreeNode node = nodes.remove();
+            if (node.left != null) {
+                nodes.add(node.left);
+                size++;
+            }
+            if (node.right != null) {
+                nodes.add(node.right);
+                size++;
+            }
+        }
+        return size;
     }
 }
