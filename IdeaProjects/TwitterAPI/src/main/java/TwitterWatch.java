8e2997381d0bdb85d28de3bb27817fea211686cd
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-05-03 12:02:07 +0200

    Posting a tweet

--- IdeaProjects/TwitterAPI/src/main/java/TwitterWatch.java
+++ IdeaProjects/TwitterAPI/src/main/java/TwitterWatch.java
@@ -1,4 +1,5 @@
 import twitter4j.Status;
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
@@ -25,5 +26,26 @@ public class TwitterWatch {
         } catch (TwitterException e) {
             e.printStackTrace();
         }
+
+        postTweet("Trying to post a tweet on Twitter");
+    }
+
+    public static void postTweet(String tweet) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("uRW6AJbfloNkLd4TuUtQtQBNh")
+                .setOAuthConsumerSecret("BuZ1m7DTj06zwafIZVhicrTaiEXJt2pfPccJ6WegXsjkzfLZ0P")
+                .setOAuthAccessToken("989077780007661571-PsYu563dRxS1gF9xUC2xOWGuPepMje9")
+                .setOAuthAccessTokenSecret("9ZjUdlujMfIMWJBIQH6d7sHKzEfGd1GGnA052ON2NC5ZP");
+
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        Status status = null;
+        try {
+            status = twitter.updateStatus(tweet);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
     }
 }
