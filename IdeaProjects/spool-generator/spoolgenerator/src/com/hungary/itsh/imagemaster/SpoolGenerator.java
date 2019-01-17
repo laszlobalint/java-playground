a9e2bbd4f441aa02a77fd706ef4cc6433ff436eb
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2019-01-17 19:45:29 +0100

    Less than 2 digit dates fixed.

--- IdeaProjects/spool-generator/spoolgenerator/src/com/hungary/itsh/imagemaster/SpoolGenerator.java
+++ IdeaProjects/spool-generator/spoolgenerator/src/com/hungary/itsh/imagemaster/SpoolGenerator.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -21,10 +22,10 @@ public class SpoolGenerator {
     private static String currencies[] = {"EUR", "HUF", "RSD", "USD", "GBP", "BRL", "AUD", "CAD", "HRK"};
 
     public static void main(String[] args) {
-	    numOfDocs = Integer.parseInt(args[0]);
-	    sourceFileExtension = args[1];
-	    attachmentFileExtension = args[2];
-	    spoolName = args[3];
+        numOfDocs = Integer.parseInt(args[0]);
+        sourceFileExtension = args[1];
+        attachmentFileExtension = args[2];
+        spoolName = args[3];
         sep = args[4];
 
         createRandomDocument();
@@ -34,10 +35,10 @@ public class SpoolGenerator {
         for (int num = 0; num < numOfDocs; num++) {
             String field1_invoiceNumber = String.valueOf((int)Math.floor(Math.random() * 9999 + 1));
             String field2_invoiceDate = getRandomDate();
-            String field3_grossAmount = String.valueOf(getRandomGrossAmount());
-            String field4_netValue = String.valueOf(Integer.parseInt(field3_grossAmount) * 0.8);
+            String field3_grossAmount = String.valueOf(Math.round(getRandomGrossAmount()));
+            String field4_netValue = String.valueOf(Math.round(Integer.parseInt(field3_grossAmount) * 0.8));
             String field5_currency = currencies[new Random().nextInt(currencies.length)];
-            String field6_taxAmount = String.valueOf(Math.floor(Integer.parseInt(field3_grossAmount) * 0.2));
+            String field6_taxAmount = String.valueOf(Math.round(Integer.parseInt(field3_grossAmount) * 0.2));
             String field7_attachment = spoolName + num + "." + attachmentFileExtension;
             String content = field1_invoiceNumber + sep + field2_invoiceDate + sep + field3_grossAmount + ".00" + sep + field4_netValue + ".00" +
                     sep + field5_currency + sep + field6_taxAmount + ".00" + sep + field7_attachment;
@@ -68,16 +69,19 @@ public class SpoolGenerator {
         gc.set(gc.YEAR, year);
         int dayOfYear = randomIntBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
         gc.set(gc.DAY_OF_YEAR, dayOfYear);
-        return (gc.get(gc.DAY_OF_MONTH)) + "." + (gc.get(gc.MONTH) + 1) + "." + (gc.get(gc.YEAR));
+
+        return new SimpleDateFormat("dd.MM.yyyy").format(gc.getTime());
     }
 
     private static int randomIntBetween(int start, int end) {
+
         return start + (int)Math.round(Math.random() * (end - start));
     }
 
     private static int getRandomGrossAmount() {
         int max = 1000000;
         int min = 500;
+
         return (int) (Math.floor(Math.random() * (max - min + 1)) + min);
     }
 }
